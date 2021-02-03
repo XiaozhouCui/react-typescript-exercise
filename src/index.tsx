4c0c5af3fbb96608a1d26174f1fb6ae7b47c60db
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-03 10:58:00 +1000

    Added GuestList component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom";
+import GuestList from "./state/GuestList";
 
 const App = () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <GuestList />
     </div>
   );
 };
